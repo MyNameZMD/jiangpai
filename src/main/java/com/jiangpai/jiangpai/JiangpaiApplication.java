62e9b090c276cf13ee40982b17651a3ffebb3dc9
MyNameZMD <1909781417@qq.com>
2019-03-02 10:38:02 +0800

    Initial commit

---
+++ src/main/java/com/jiangpai/jiangpai/JiangpaiApplication.java
@@ -0,0 +1,13 @@
+package com.jiangpai.jiangpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiangpaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiangpaiApplication.class, args);
+	}
+
+}
